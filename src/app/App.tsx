0b19805279100aaacde4e7c98153f87d08c73168
kwlodarczyk0 <wlodarczyk.krystian00@gmail.com>
2023-04-15 12:27:14 +0200

    init react

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <h2>Hello world</h2>
+    </div>
+  )
+}
+
+export default App
